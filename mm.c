f1fd391260ebce08b84058c79ad1de5c9df2402f
이재혁 <cncnvmvm@naver.com>
2026-04-13 11:27:33 +0900

    매크로, 헬퍼함수 선언

--- mm.c
+++ mm.c
@@ -40,15 +40,44 @@ team_t team = {
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+// 이건 삭제 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<< 12)
+#define MAX(x,y) ((x)>(y) ? (x):(y))
+#define PACK(size,alloc) ((size)|(alloc))
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p,val) (*(unsigned int *)(p)=(val))
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define HDRP(bp) ((char *)(bp)-WSIZE)
+#define FTRP(bp) ((char *)(bp)+GET_SIZE(HDRP(bp))-DSIZE)
+#define NEXT_BLKP(bp) ((char *)(bp)+GET_SIZE(HDRP(bp)))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 
+// 헬퍼 함수 선언 부분
+static char *heap_listp;
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-
+//
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    if ((heap_listp=mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0); //정렬용 4바이트 패딩
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE,1)); //프롤로그 헤더
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE,1)); //프롤로그 푸터
+    PUT(heap_listp + (3 * WSIZE), PACK(0,1)); //에필로그 블럭의 크기는 4이지만, 헤더에 들어있는 size는 0
+    heap_listp+=(2 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE)==NULL)
+        return -1;
     return 0;
 }
 
